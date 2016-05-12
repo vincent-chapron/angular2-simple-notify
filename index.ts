400f83c947a2439375c009d0735e593449fc5f4c
Vincent Chapron <vincent@chapron.io>
2016-05-12 18:16:45 +0200

    Add index

---
+++ index.ts
@@ -0,0 +1,7 @@
+import {NotifyService as _notifyService} from './notify.service'
+import {NotifyComponent as _notifyComponent} from './notify.component'
+import {Notify as _notify} from './notify'
+
+export let NotifyService = _notifyService;
+export let NotifyComponent = _notifyComponent;
+export let Notify = _notify;
