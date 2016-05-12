c6d91481af7d29a9d80cb991cba69387d9170395
Vincent Chapron <vincent@chapron.io>
2016-05-12 18:36:49 +0200

    use export * from

--- index.ts
+++ index.ts
@@ -1,7 +1,3 @@
-import {NotifyService as _notifyService} from './notify.service'
-import {NotifyComponent as _notifyComponent} from './notify.component'
-import {Notify as _notify} from './notify'
-
-export let NotifyService = _notifyService;
-export let NotifyComponent = _notifyComponent;
-export let Notify = _notify;
+export * from './notify.service'
+export * from './notify.component'
+export * from './notify.interface'
